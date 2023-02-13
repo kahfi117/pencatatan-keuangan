383d98a19caf2d0df400f094c1fc4f5953aec28b
Muhammad Ashabul Kahfi <ash.kahfi20@gmail.com>
2023-02-13 21:24:02 +0800

    update dashboards

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Penjualan;
+use App\Models\BarangMasuk;
+use App\Models\ListingFee;
+use App\Models\GajiKaryawan;
+use App\Models\Operasional;
+use App\Models\SumberNonCash;
+use App\Models\Tenant;
 
 class DashboardController extends Controller
 {
     public function index(){
         $penjualan = Penjualan::all();
+
+        $sb = SumberNonCash::all();
+
+        $tenant = Tenant::all();
+
+        $fee = ListingFee::all();
+
+        $gaji = GajiKaryawan::all();
+
+        $operasional = Operasional::all();
+
+        $barang = BarangMasuk::all();
+
+        // $barang_kredit = BarangMasuk::where('status', '=', 'On Kredit')->get();
+
+        foreach($barang as $bl){
+            $lunas[] = $bl->nominal_cash;
+            $harga[] = $bl->harga;
+            $kredit[] = $bl->nominal_kredit;
+        }
+
+        // foreach($barang_kredit as $bk){
+        //     $kredit[] = $bk->nominal_kredit or null;
+        //     $harga_kredit[] = $bk->harga;
+        // }
+
+        
+
+        $jumlah_lunas = array_sum($lunas);
+
+        $jumlah_kredit = array_sum($kredit);
+
+        $total_barang_masuk = array_sum($harga);
 
         foreach($penjualan as $pj){
             $pjl_penjualan[] = $pj->nominal_penjualan;
@@ -16,14 +55,38 @@ class DashboardController extends Controller
             $pjl_kembalian_konsumen[] = $pj->nominal_kembalian_konsumen;
             $pjl_modal_kasir[] = $pj->nominal_modal_kasir;
         }
-        $jumlah_penjualan =array_sum($pjl_penjualan);
-        $jumlah_laba_rugi =array_sum($pjl_laba_rugi);
-        $jumlah_kembalian_konsumen =array_sum($pjl_kembalian_konsumen);
-        $jumlah_modal_kasir =array_sum($pjl_modal_kasir);
+
+        foreach($sb as $sumber){
+            $cek[] = $sumber->mandiri + $sumber->bni;
+        }
+
+        foreach($tenant as $tn){
+            $ten[] = $tn->nominal;
+        }
+
+        foreach($gaji as $gj){
+            $gji[] = $gj->nominal;
+        }
+
+        foreach($operasional as $op){
+            $oper[] = $op->nominal;
+        }
+
+        foreach($fee as $f){
+            $lf[] = $f->nominal;
+        }
+
+
+        $jumlah_penjualan = array_sum($pjl_penjualan);
+        $jumlah_laba_rugi = array_sum($pjl_laba_rugi);
+        $jumlah_kembalian_konsumen = array_sum($pjl_kembalian_konsumen);
+        $jumlah_modal_kasir = array_sum($pjl_modal_kasir);
 
         $total_penjualan = $jumlah_penjualan + $jumlah_modal_kasir - $jumlah_kembalian_konsumen - $jumlah_laba_rugi;
 
 
-        return view('contents.dashboard',compact('total_penjualan'));
+
+
+        return view('contents.dashboard',compact('total_penjualan', 'total_barang_masuk', 'jumlah_lunas', 'jumlah_kredit'));
     }
 }
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -10,22 +10,22 @@
             <li class="menu-header">Dashboard</li>
             <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('dashboard') }}"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                    href="{{ route('dashboard') }}"><i class="fa-solid fa-shop"></i><span>Dashboard</span></a>
             </li>
             <li class="{{ Request::is('modal*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('modal.index') }}"><i class="fas fa-fire"></i><span>Modal</span></a>
+                    href="{{ route('modal.index') }}"><i class="fa fa-usd"></i><span>Modal</span></a>
             </li>
 
             <li class="menu-header">Pemasukan Penjualan</li>
 
             <li class="{{ Request::is('penjualan*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('penjualan.index') }}"><i class="fas fa-wallet"></i> <span>Penjualan</span></a>
+                    href="{{ route('penjualan.index') }}"><i class="fa-solid fa-cart-shopping"></i> <span>Penjualan</span></a>
             </li>
             <li class="{{ Request::is('non-tunai') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('non-tunai.index') }}"><i class="fas fa-wallet"></i> <span>Non Tunai</span></a>
+                    href="{{ route('non-tunai.index') }}"><i class="fa fa-credit-card-alt"></i> <span>Non Tunai</span></a>
             </li>
 
             {{-- <li class="{{ Request::is('blank-page') ? 'active' : '' }}">
@@ -36,7 +36,7 @@
             <li class="menu-header">Barang</li>
             <li class="{{ Request::is('barang-masuk*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('bm.index') }}"><i class="far fa-plug"></i> <span>Data Barang Masuk</span></a>
+                    href="{{ route('bm.index') }}"><i class="fa-solid fa-file-import"></i> <span>Data Barang Masuk</span></a>
             </li>
             <li class="{{ Request::is('barang-kredit*') ? 'active' : '' }}">
                 <a class="nav-link"
@@ -46,25 +46,25 @@
             <li class="menu-header">Lainnya</li>
             <li class="{{ Request::is('gaji-pegawai*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('gaji-pegawai.index') }}"><i class="fas fa-pencil-ruler">
+                    href="{{ route('gaji-pegawai.index') }}"><i class="fa fa-address-card">
                     </i> <span>Gaji Karyawan</span>
                 </a>
             </li>
             <li class="{{ Request::is('operasional*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('operasional.index') }}"><i class="fas fa-pencil-ruler">
+                    href="{{ route('operasional.index') }}"><i class="fa-solid fa-users-gear">
                     </i> <span>Operasional</span>
                 </a>
             </li>
             <li class="{{ Request::is('sewa-tenant*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('sewa-tenant.index') }}"><i class="fas fa-pencil-ruler">
+                    href="{{ route('sewa-tenant.index') }}"><i class="fa-solid fa-shop">
                     </i> <span>Sewa Tenant</span>
                 </a>
             </li>
             <li class="{{ Request::is('listing-fee*') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ route('listing-fee.index') }}"><i class="fas fa-pencil-ruler">
+                    href="{{ route('listing-fee.index') }}"><i class="fa-solid fa-file-invoice">
                     </i> <span>Listing Fee</span>
                 </a>
             </li>
